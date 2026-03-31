b876bb3d67c7be79dcda1eba5245ba6a4003f531
= <=>
2026-03-31 11:22:28 +0300

    first commit

---
+++ workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
@@ -0,0 +1,18 @@
+package backend.workoutplanner.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+// käsittelee clientin pyynnöt/requestit ja välittää takaisin
+// vastaukset/responset
+public class PlannerController {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String returnMessage() {
+        return "Workout Planner";
+    }
+
+}
