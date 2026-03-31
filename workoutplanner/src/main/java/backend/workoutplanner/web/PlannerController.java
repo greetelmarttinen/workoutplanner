ec4b92a6ba8ebc87ba5ca4f38aed2941360ed8f5
= <=>
2026-03-31 12:58:11 +0300

    ylimääräsen importin poisto controllerista

--- workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
+++ workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
@@ -2,7 +2,6 @@ package backend.workoutplanner.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 // käsittelee clientin pyynnöt/requestit ja välittää takaisin
