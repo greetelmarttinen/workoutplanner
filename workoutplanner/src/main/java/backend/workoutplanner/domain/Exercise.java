93fcba7a42558086a95a4d553b010321def5b8bc
= <=>
2026-03-31 13:37:07 +0300

    exercise entity luotu

--- workoutplanner/src/main/java/backend/workoutplanner/domain/Exercise.java
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/Exercise.java
@@ -1,5 +1,61 @@
 package backend.workoutplanner.domain;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Exercise {
+
+    @Id // yksilöivä id / PK
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long exerciseId;
+
+    private String name;
+    private String muscleGroup;
+
+    // parametriton konstruktori
+    public Exercise() {
+
+    }
+
+    // parametrillinen konstruktori
+    public Exercise(String name, String muscleGroup) {
+        this.name = name;
+        this.muscleGroup = muscleGroup
+    }
+
+    // getterit ja setterit
+    public Long getExerciseId() {
+        return exerciseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setExerciseId(Long exerciseId) {
+        this.exerciseId = exerciseId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Exercise [exerciseId=" + exerciseId + ", name=" + name + ", muscleGroup=" + muscleGroup + "]";
+    }
 
 }
