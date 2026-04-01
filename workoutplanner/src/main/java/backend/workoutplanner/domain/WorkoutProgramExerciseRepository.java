f4709d2ad22d6e31948614b1206e850454c0790c
= <=>
2026-04-01 12:00:48 +0300

    uusien tiedostojen pushaus

---
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/WorkoutProgramExerciseRepository.java
@@ -0,0 +1,7 @@
+package backend.workoutplanner.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WorkoutProgramExerciseRepository extends CrudRepository<WorkoutProgramExercise, Long> {
+
+}
---
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/WorkoutProgramRepository.java
@@ -0,0 +1,7 @@
+package backend.workoutplanner.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WorkoutProgramRepository extends CrudRepository<WorkoutProgram, Long> {
+
+}
