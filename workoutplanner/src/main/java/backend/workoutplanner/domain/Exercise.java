fb15aa5132064f257cb65b664cd720825b766a5b
= <=>
2026-04-01 10:53:08 +0300

    id:llä oli väärä import --> korjattu oikeaksi

--- workoutplanner/src/main/java/backend/workoutplanner/domain/Exercise.java
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/Exercise.java
@@ -1,10 +1,9 @@
 package backend.workoutplanner.domain;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Exercise {
