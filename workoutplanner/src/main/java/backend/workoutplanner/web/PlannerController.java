b34d7ac8969edd610fcd544ab6f95cfedc0e24eb
= <=>
2026-04-01 10:54:00 +0300

    'kotisivujen' endpointien lisäys

--- workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
+++ workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
@@ -3,12 +3,20 @@ package backend.workoutplanner.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import backend.workoutplanner.domain.ExerciseRepository;
+
 @Controller
 // käsittelee clientin pyynnöt/requestit ja välittää takaisin
 // vastaukset/responset
 public class PlannerController {
+    private ExerciseRepository exerciseRepository;
 
-    @GetMapping("/index")
+    // konstruktorin injektointi
+    public PlannerController(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+
+    @GetMapping({ "/", "/index" })
     public String mainPage() {
         return "index"; // index.html
     }
