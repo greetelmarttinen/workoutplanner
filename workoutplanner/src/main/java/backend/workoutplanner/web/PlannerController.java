f7c959f66343ec9614d40acac78fb70ee1287cc4
= <=>
2026-04-03 19:26:27 +0300

    ohjelmasisällön luonti + uuden liikkeen lisäys -metodit kontrolleriin

--- workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
+++ workoutplanner/src/main/java/backend/workoutplanner/web/PlannerController.java
@@ -1,5 +1,6 @@
 package backend.workoutplanner.web;
 
+import backend.workoutplanner.domain.WorkoutProgramExerciseRepository;
 import backend.workoutplanner.domain.WorkoutProgramRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,23 +10,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import backend.workoutplanner.domain.ExerciseRepository;
 import backend.workoutplanner.domain.WorkoutProgram;
+import backend.workoutplanner.domain.WorkoutProgramExercise;
 
 @Controller
 // käsittelee clientin pyynnöt/requestit ja välittää takaisin
 // vastaukset/responset
 public class PlannerController {
+    private final WorkoutProgramExerciseRepository workoutProgramExerciseRepository;
     private final WorkoutProgramRepository workoutProgramRepository;
     private ExerciseRepository exerciseRepository;
 
     // konstruktorin injektointi
-    public PlannerController(ExerciseRepository exerciseRepository, WorkoutProgramRepository workoutProgramRepository) {
+    public PlannerController(ExerciseRepository exerciseRepository, WorkoutProgramRepository workoutProgramRepository,
+            WorkoutProgramExerciseRepository workoutProgramExerciseRepository) {
         this.exerciseRepository = exerciseRepository;
         this.workoutProgramRepository = workoutProgramRepository;
+        this.workoutProgramExerciseRepository = workoutProgramExerciseRepository;
     }
 
     // 'kotisivu'
     @GetMapping({ "/", "/index" })
-    public String mainPage() {
+    public String mainPage(Model model) {
+        model.addAttribute("workoutPrograms", workoutProgramRepository.findAll());
         return "index"; // index.html
     }
 
@@ -41,7 +47,21 @@ public class PlannerController {
     @PostMapping("/saveprogram")
     public String save(@ModelAttribute WorkoutProgram workoutProgram) {
         workoutProgramRepository.save(workoutProgram);
-        return "redirect../index"; // index.html
+        return "redirect:/index"; // index.html
+    }
+
+    // ohjelmasisällön luonti
+    @GetMapping("/openprogram")
+    public String openProgram(Model model) {
+        model.addAttribute("workoutProgramExercise", workoutProgramExerciseRepository.findAll());
+        return "openprogram"; // openprogram.html
+    }
+
+    // liikkeiden tallennus ohjelmaan
+    @PostMapping("/addexercise")
+    public String save(@ModelAttribute WorkoutProgramExercise addExercise) {
+        workoutProgramExerciseRepository.save(addExercise);
+        return "redirect:/openprogram"; // openprogram
     }
 
 }
