b2329ba382dff449a2696a7979e7d38ff94f6d70
= <=>
2026-04-03 19:30:24 +0300

    pieniä muokkauksia

--- workoutplanner/src/main/java/backend/workoutplanner/domain/WorkoutProgramRepository.java
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/WorkoutProgramRepository.java
@@ -1,7 +1,10 @@
 package backend.workoutplanner.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface WorkoutProgramRepository extends CrudRepository<WorkoutProgram, Long> {
 
+    List<WorkoutProgram> findByName(String name);
 }
