79d492de8086203c6783b63a85434a6b6f4cf033
= <=>
2026-04-03 19:25:16 +0300

    kommentin lisäys

--- workoutplanner/src/main/java/backend/workoutplanner/domain/WorkoutProgramExerciseRepository.java
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/WorkoutProgramExerciseRepository.java
@@ -3,5 +3,7 @@ package backend.workoutplanner.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WorkoutProgramExerciseRepository extends CrudRepository<WorkoutProgramExercise, Long> {
+    // tähän pitäis lisätä joku crud toiminto, mil löytyis ne liikkee jo
+    // pitäiskö se olla findbyname?
 
 }
