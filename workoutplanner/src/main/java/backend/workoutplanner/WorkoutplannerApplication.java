bcab92defe40d9b79385fd0e1e5e73a11a11557c
= <=>
2026-04-08 14:54:20 +0300

    lisäys: käyttäjätiedot/kirjautumistiedot

--- workoutplanner/src/main/java/backend/workoutplanner/WorkoutplannerApplication.java
+++ workoutplanner/src/main/java/backend/workoutplanner/WorkoutplannerApplication.java
@@ -1,5 +1,6 @@
 package backend.workoutplanner;
 
+import backend.workoutplanner.domain.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 import backend.workoutplanner.domain.Exercise;
 import backend.workoutplanner.domain.ExerciseRepository;
+import backend.workoutplanner.domain.User;
 import backend.workoutplanner.domain.WorkoutProgram;
 import backend.workoutplanner.domain.WorkoutProgramExercise;
 import backend.workoutplanner.domain.WorkoutProgramExerciseRepository;
@@ -18,7 +20,12 @@ import backend.workoutplanner.domain.WorkoutProgramRepository;
 @SpringBootApplication
 public class WorkoutplannerApplication {
 
+	private final UserRepository userRepository;
 	private static final Logger log = LoggerFactory.getLogger(WorkoutplannerApplication.class);
+
+	WorkoutplannerApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutplannerApplication.class, args);
@@ -46,6 +53,17 @@ public class WorkoutplannerApplication {
 			wpRepo.save(new WorkoutProgram("Leg day 1", "1.4.2026"));
 			wpRepo.save(new WorkoutProgram("Upper body", "1.4.2026"));
 			wpRepo.save(new WorkoutProgram("Leg day 2", "1.4.2026"));
+
+			// käyttäjien luonti
+			User user1 = new User("user1", "$2a$10$UURCCD.X2b/EQvadUPaR3.tWDH5P6pQ2fQ8MG.sr1RkWatQZ6pq4W",
+					"user1@email.com", "USER");
+			User user2 = new User("admin", "$2a$10$8TKtqPsy5F.4xSYinVNrh.zDy9yQ.pJUCGwSz9dt6GbCwGo3T9te.",
+					"admin@email.com", "ADMIN");
+			User user3 = new User("user2", "$2a$10$PhYsDhM7A3QHlFT/uzkZpO6R918gl4Cj23yiEscxsVPF9hMlB1g6K",
+					"user2@email.com", "USER");
+			userRepository.save(user1);
+			userRepository.save(user2);
+			userRepository.save(user3);
 		};
 	}
 
