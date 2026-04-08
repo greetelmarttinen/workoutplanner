c44e660f76ba3fe5625d687ae7a4cf74fb6a79d8
= <=>
2026-04-08 14:55:10 +0300

    luotu user -entity

--- workoutplanner/src/main/java/backend/workoutplanner/domain/User.java
+++ workoutplanner/src/main/java/backend/workoutplanner/domain/User.java
@@ -1,5 +1,92 @@
 package backend.workoutplanner.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long userId;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    // parametriton konstruktori
+    public User() {
+
+    }
+
+    // parametrillinen konstruktori
+    public User(String username, String passwordHash, String email, String role) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.email = email;
+        this.role = role;
+    }
+
+    // getters
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // setters
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "User [userId=" + userId + ", username=" + username + ", passwordHash=" + passwordHash + ", email="
+                + email + ", role=" + role + "]";
+    }
 
 }
